5fa4dfc1359cd78fa037a03b98a935491b858ac0
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2022-08-19 16:16:19 -0400

    controladores y parametros

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -18,5 +18,37 @@ export class AppController {
   @Get('/ruta/')
   hello() {
     return 'Que hay bro';
+  }
+
+  // con parámetros
+  // @Get('/products/:productoId')
+  // getProduct(@Param() params: any) {
+  //   return `product ${params.productoId}`;
+  // }
+
+  //Primero los estáticos y después los dinámicos para que no choquen las rutas con Id
+  @Get('/products/filter')
+  getProductFilter() {
+    return `yo soy un filtro`;
+  }
+  // con parámetros de otra manera rápida directa
+  @Get('/products/:productId')
+  getProduct(@Param('productId') productId: string) {
+    return `product ${productId}`;
+  }
+
+  // rutas con id de parámetros
+  @Get('/categories/:id/products/:productId')
+  getCategory(@Param('productId') productId: string, @Param('id') id: string) {
+    return `product ${productId} and ${id}`;
+  }
+
+  @Get('products')
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 50,
+    @Query('brand') brand: string,
+  ) {
+    return `products: Limit=> ${limit}$ offset =>${offset} brand =>${brand}`;
   }
 }
